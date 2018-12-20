6e51743178b469c3454d609ffbc5914b7b295501
Chris Atkin <atkinchris@gmail.com>
2018-12-20 18:14:48 +0000

    Fix master branch selector

--- esp-idf.rb
+++ esp-idf.rb
@@ -5,7 +5,7 @@
 class EspIdf < Formula
   desc "ESP-IDF"
   homepage "https://github.com/espressif/esp-idf"
-  url "https://github.com/espressif/esp-idf.git", :using => :git, :branch => :master
+  url "https://github.com/espressif/esp-idf.git", :using => :git, :branch => "master"
   depends_on "xtensa-esp32-elf"
   depends_on "python@2" => ['pyserial']
 
